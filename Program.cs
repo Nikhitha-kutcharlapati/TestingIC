4d5363b8d86a37d5f729ef442e8d3807e1e6e589
Nikhitha-kutcharlapati <nikhitha.knv@gmail.com>
2024-03-21 23:25:45 +1300

    First Commit of Initial project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+IWebElement username = driver.FindElement(By.Id("UserName"));
+username.SendKeys("hari");
+IWebElement password = driver.FindElement(By.Id("Password"));
+password.SendKeys("123123");
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginbutton.Click();
+
+Console.WriteLine(" User logged in sucessfully");
+driver.Quit();
